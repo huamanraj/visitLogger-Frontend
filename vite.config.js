2a392ccf4a37bf0af2ceb613cdfe86ac08d89dda
Aman Raj <113578582+huamanraj@users.noreply.github.com>
2025-01-29 01:37:04 +0530

    routes added

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/",  // Make sure the base path is correctly set
+  build: {
+    outDir: "dist",  // Ensure the output directory is "dist"
+  },
 })
